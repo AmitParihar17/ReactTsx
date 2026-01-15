d0e81e4876f83ca8e32e80eed18bbeb9188ad81c
AmitParihar17 <amitparihar1208@gmail.com>
2026-01-15 22:54:13 +0530

    context api using  TS

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -2,9 +2,15 @@
 // import Person from "./components/Person"
 // import { PersonList } from "./components/PersonList";
 
-import Button from "./components/Button"
-import Container from "./components/Container";
-import Input from "./components/Input";
+// import Button from "./components/Button"
+// import Container from "./components/Container";
+// import Input from "./components/Input";
+
+// import { Box } from "./context/Box";
+// import { ThemeContextProvider } from "./context/ThemeContext";
+
+import User from "./context/User";
+import { UserContextProvider } from "./context/UserContext";
 
 // import { Greet } from "./components/Greet";
 // import Heading from "./components/Heading"
@@ -60,17 +66,43 @@ import Input from "./components/Input";
 
 
 
+// export const App = () => {
+//   return (
+//     <div>
+//       <Button  handleClick={(event,id) => {
+//       console.log('Button Clicked',event,id);   
+//       }}/>
+//       <Input  value="" handleChange={(event) => console.log(event)}/>
+//       <Container styles={{border : "2px solid red",padding : "1rem"}} />
+//     </div>
+//   )
+// }
+
+
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <ThemeContextProvider>
+//         <Box />
+//       </ThemeContextProvider>
+//     </div>
+//   )
+// }
+
 export const App = () => {
   return (
     <div>
-      <Button  handleClick={(event,id) => {
-      console.log('Button Clicked',event,id);   
-      }}/>
-      <Input  value="" handleChange={(event) => console.log(event)}/>
-      <Container styles={{border : "2px solid red",padding : "1rem"}} />
+    <UserContextProvider>
+       <User />
+    </UserContextProvider>
+
+
     </div>
   )
 }
+
+
 
 
 
---
+++ typescript/src/context/User.tsx
@@ -0,0 +1,30 @@
+import {  useContext } from "react";
+import { UserContext } from "./UserContext";
+
+
+const User = () => {
+    const userContext = useContext(UserContext)
+    const handleLogin = () => {
+        if(userContext){
+            userContext.setUser({
+                name : "Amit",
+                email : "amit@example.com"
+            })
+        }
+    }
+    const handleLogout = () => {
+        if(userContext){
+            userContext.setUser(null)
+        }
+    }
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <p>User name is {userContext?.user?.name}</p>
+      <p>User email is {userContext?.user?.email}</p>
+    </div>
+  );
+}
+
+export default User
